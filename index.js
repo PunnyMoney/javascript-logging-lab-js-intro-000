98b5f3318a23fb630773faff0dbcc45d0c14fc01
Penelope Jungreis <penelope.jungreis@gmail.com>
2020-05-18 17:10:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ console.warn("Are you sure you want to cancel this transaction?");
 
 // declare the variable
 var word
- 
+
 // assign a value to the variable
 word = 'bird'
- 
+
 console.log(word) // 'bird'
- 
+
 // assign another value to the variable
 word = 'dog'
- 
+
 console.log(word) // 'dog'
