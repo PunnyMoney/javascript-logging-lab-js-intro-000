1de85187f902dbd0fad05f021ccdd38658d84f74
Penelope Jungreis <penelope.jungreis@gmail.com>
2020-05-18 17:10:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 console.error("Invalid PIN entered.");
 console.log("Please insert your card.");
 console.warn("Are you sure you want to cancel this transaction?");
+
+// declare the variable
+var word
+ 
+// assign a value to the variable
+word = 'bird'
+ 
+console.log(word) // 'bird'
+ 
+// assign another value to the variable
+word = 'dog'
+ 
+console.log(word) // 'dog'
